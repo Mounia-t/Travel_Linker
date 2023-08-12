40b4ba95f1ff21f1efd842c50f067b7a92daff2f
SerdarU <s-ustundag@hotmail.com>
2023-08-12 03:13:38 +0200

    cart

--- src/main/java/travelLinker/entity/Cart.java
+++ src/main/java/travelLinker/entity/Cart.java
@@ -1,10 +1,13 @@
 package travelLinker.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -14,8 +17,19 @@ public class Cart {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	@OneToMany(mappedBy = "cart")
+	private List<Item> items;
+
 	@OneToOne
 	@JoinColumn(name = "customer_id_fk")
 	private Customer customer;
+
+	@OneToOne
+	@JoinColumn(name = "travel_planer_id_fk")
+	private TravelPlanner travelPlanner;
+
+	@OneToOne
+	@JoinColumn(name = "partner_id_fk")
+	private Partner partner;
 
 }
