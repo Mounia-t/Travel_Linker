c99e3763bdb7939050b88d47896527bc631b1afc
Fred <frederic.dupin@gmail.com>
2023-08-18 11:35:39 +0200

    Crea dao et entity Template

---
+++ src/main/java/travelLinker/controller/TemplateControllerBean.java
@@ -0,0 +1,42 @@
+package travelLinker.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+
+import travelLinker.dao.TemplateDao;
+import travelLinker.entity.Template;
+
+@ManagedBean
+@SessionScoped
+public class TemplateControllerBean {
+
+	@Inject
+    private TemplateDao templateDao;
+
+    private Template selectedTemplate;
+
+    public String loadTemplate(Long templateId) {
+        selectedTemplate = templateDao.getTemplateById(templateId);
+        return "userHomePage.xhtml"; // Nom de la vue de la page d'accueil personnalisée
+    }
+
+	public TemplateDao getTemplateDao() {
+		return templateDao;
+	}
+
+	public void setTemplateDao(TemplateDao templateDao) {
+		this.templateDao = templateDao;
+	}
+
+	public Template getSelectedTemplate() {
+		return selectedTemplate;
+	}
+
+	public void setSelectedTemplate(Template selectedTemplate) {
+		this.selectedTemplate = selectedTemplate;
+	}
+    
+    
+    
+}
