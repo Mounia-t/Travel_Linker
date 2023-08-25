2dcf52a864fe611eef7c18a1eed578dcf7bcc5da
SerdarU <s-ustundag@hotmail.com>
2023-08-25 21:25:48 +0200

    qq modifs

--- src/main/java/travelLinker/entity/Restaurant.java
+++ src/main/java/travelLinker/entity/Restaurant.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Restaurant extends Service {
@@ -11,6 +13,10 @@ public class Restaurant extends Service {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "journey_id_fk")
+	private Journey journey;
 
 	private String typeOfRestaurant;
 
